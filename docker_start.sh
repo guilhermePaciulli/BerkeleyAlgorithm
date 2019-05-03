816f9fbb4aad65d118575068fcf3874a049dd1fb
Guilherme Paciulli <guilherme.paciulli@icloud.com>
2019-05-03 17:12:08 -0300

    updates to docker file

--- docker_start.sh
+++ docker_start.sh
@@ -1,9 +1,11 @@
 docker run -it -d --name slave1 python bash
 docker run -it -d --name slave2 python bash
 docker run -it -d --name slave3 python bash
-docker run -it -d --name master python bash
 
 docker exec -t slave1 bash -c 'git clone https://github.com/guilhermePaciulli/BerkeleyAlgorithm.git;cd BerkeleyAlgorithm;git pull;cd test;bash slave_1.sh'
+sleep 1
 docker exec -t slave2 bash -c 'git clone https://github.com/guilhermePaciulli/BerkeleyAlgorithm.git;cd BerkeleyAlgorithm;git pull;cd test;bash slave_2.sh'
+sleep 1
 docker exec -t slave3 bash -c 'git clone https://github.com/guilhermePaciulli/BerkeleyAlgorithm.git;cd BerkeleyAlgorithm;git pull;cd test;bash slave_3.sh'
-docker exec -t master bash -c 'git clone https://github.com/guilhermePaciulli/BerkeleyAlgorithm.git;cd BerkeleyAlgorithm;git pull;cd test;bash master.sh'
+sleep 1
+bash test/master.sh
