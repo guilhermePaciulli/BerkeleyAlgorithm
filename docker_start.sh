0aa624c716f3c13e8c8f16134648e6f774a53878
Guilherme Paciulli <guilherme.paciulli@icloud.com>
2019-05-06 20:48:54 -0300

    advances

--- docker_start.sh
+++ docker_start.sh
@@ -1,4 +1,4 @@
-docker run -it -d -p 9000:9000 --name slave1 python bash
+docker run -it -d -p 127.0.0.1:9000:9000/tcp --name slave1 python bash
 # docker run -it -d -p 127.0.0.1:9001:9001/tcp --name slave2 python bash
 # docker run -it -d -p 127.0.0.1:9002:9002/tcp --name slave3 python bash
 
@@ -9,4 +9,4 @@ sleep 1
 # docker exec -t -d slave3 bash -c 'git clone https://github.com/guilhermePaciulli/BerkeleyAlgorithm.git;cd BerkeleyAlgorithm;git pull;cd test;bash slave_3.sh'
 # sleep 1
 
-bash test/master.sh
+# bash test/master.sh
